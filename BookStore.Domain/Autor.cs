ba1e5ddabf7588969588ac39a02fa0fca7dc8b83
Bernard Johnson <bernardjohnson@outlook.com.br>
2017-05-10 10:57:41 -0300

    Versão 1

---
+++ BookStore.Domain/Autor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+namespace BookStore.Domain
+{
+    public class Autor
+    {
+        public Autor()
+        {
+            this.Livros = new List<Livro>();
+        }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public ICollection<Livro> Livros { get; set; }
+    }
+}
---
+++ BookStore/Mapping/AutorMap.cs
@@ -0,0 +1,20 @@
+﻿using BookStore.Domain;
+using System.Data.Entity.ModelConfiguration;
+
+namespace BookStore.Mapping
+{
+    public class AutorMap : EntityTypeConfiguration<Autor>
+    {
+        public AutorMap()
+        {
+            ToTable("Autor");
+            Property(x => x.Nome)
+                .HasMaxLength(30)
+                .IsRequired();
+            HasMany(x => x.Livros)
+                .WithMany(x => x.Autores)
+                .Map(x => x.ToTable("LivroAutor"));
+
+        }
+    }
+}
